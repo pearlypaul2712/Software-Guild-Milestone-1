9eb388981eb1ab3d2fccd63d7ac411d83b2c0472
pearlypaul2712 <58404869+pearlypaul2712@users.noreply.github.com>
2020-01-04 20:16:02 -0600

    Add files via upload

---
+++ Software-Guild-Milestone-1/MaybeItLovesMe/MaybeItLovesMe/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+/**
+ * Author : PEarly Paul
+ * Change your daisy-prediction program from before, to have a RANDOM number of petals for the daisy (range is usually from 13 - 89 petals).
+If the last petal ends on "it loves me NOT" print "Awwww, bummer." otherwise print "Oh, wow! It really LOVES me!"
+    
+     * **/
+
+
+namespace MaybeItLovesMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random randomizer = new Random();
+
+            int input = randomizer.Next(13, 89);
+            String out1 = "It LOVES me!";
+            String out2 = "It loves me NOT!";
+            String output;
+
+            int count = 0;
+
+            while (input >= 13)
+            {
+
+                count++;
+                if (count % 2 == 0)
+
+                {
+                    Console.WriteLine(out1);
+                    output = out1;
+                }
+                else
+                {
+                    Console.WriteLine(out2);
+                    output = out2;
+                }
+
+                
+
+                if (output.Equals(out1) && input==13)
+                {
+                    Console.WriteLine("Oh, wow! It really LOVES me!");
+                }
+                else if ((output.Equals(out2) && input == 13))
+                {
+
+                    Console.WriteLine("Awwww, bummer.");
+                }
+                input--;
+                
+            }
+
+       
+        }
+    }
+}
